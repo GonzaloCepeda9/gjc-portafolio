d1e90aaaad74f5a13f4d91d35dd781475eaa6d4f
Gonzalo Cepeda <gjcepeda9@gmail.com>
2022-09-28 12:26:27 -0300

    Detalles

--- src/componentes/Secciones/Seccion3.js
+++ src/componentes/Secciones/Seccion3.js
@@ -17,31 +17,31 @@ function Seccion3 () {
         <h3 className="titulo-secundario">Proyectos Básicos</h3>
         <div className="proyectos-basicos">
             <div className="contenedor-proyecto">
-              <img className="imagen-proyecto" src={Testimonios} alt="" />
+              <img className="imagen-proyecto" src={Tareas} alt="" />
               <div className="links-proyectos">
-                <a href="https://testimonios-gjc.netlify.app/" target="blank1" className="link-proyecto link-web">Ir a la página web<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
-                <a href="https://github.com/GonzaloCepeda9/freeCodeCamp-testimonios" target="blank2" className="link-proyecto">Ver código en GitHub<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
-              </div>
-            </div>
-            <div className="contenedor-proyecto">
-              <img className="imagen-proyecto" src={ContadorClicks} alt="" />
-              <div className="links-proyectos">
-                <a href="https://contadorclicks-gjc.netlify.app/" target="blank3" className="link-proyecto link-web">Ir a la página web<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
-                <a href="https://github.com/GonzaloCepeda9/freeCodeCamp-contadorClicks" target="blank4" className="link-proyecto">Ver código en GitHub<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
+                <a href="https://tareas-gjc.netlify.app/" target="blank1" className="link-proyecto link-web">Ir a la página web<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
+                <a href="https://github.com/GonzaloCepeda9/freeCodeCamp-tareas" target="blank2" className="link-proyecto">Ver código en GitHub<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
               </div>
             </div>
             <div className="contenedor-proyecto">
               <img className="imagen-proyecto" src={Calculadora} alt="" />
               <div className="links-proyectos">
-                <a href="https://calculadora-gjc.netlify.app/" target="blank5" className="link-proyecto link-web">Ir a la página web<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
-                <a href="https://github.com/GonzaloCepeda9/freeCodeCamp-calculadora" target="blank6" className="link-proyecto">Ver código en GitHub<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
+                <a href="https://calculadora-gjc.netlify.app/" target="blank3" className="link-proyecto link-web">Ir a la página web<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
+                <a href="https://github.com/GonzaloCepeda9/freeCodeCamp-calculadora" target="blank4" className="link-proyecto">Ver código en GitHub<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
               </div>
             </div>
             <div className="contenedor-proyecto">
-              <img className="imagen-proyecto" src={Tareas} alt="" />
+              <img className="imagen-proyecto" src={ContadorClicks} alt="" />
               <div className="links-proyectos">
-                <a href="https://tareas-gjc.netlify.app/" target="blank7" className="link-proyecto link-web">Ir a la página web<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
-                <a href="https://github.com/GonzaloCepeda9/freeCodeCamp-tareas" target="blank8" className="link-proyecto">Ver código en GitHub<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
+                <a href="https://contadorclicks-gjc.netlify.app/" target="blank5" className="link-proyecto link-web">Ir a la página web<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
+                <a href="https://github.com/GonzaloCepeda9/freeCodeCamp-contadorClicks" target="blank6" className="link-proyecto">Ver código en GitHub<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
+              </div>
+            </div>
+            <div className="contenedor-proyecto">
+              <img className="imagen-proyecto" src={Testimonios} alt="" />
+              <div className="links-proyectos">
+                <a href="https://testimonios-gjc.netlify.app/" target="blank7" className="link-proyecto link-web">Ir a la página web<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
+                <a href="https://github.com/GonzaloCepeda9/freeCodeCamp-testimonios" target="blank8" className="link-proyecto">Ver código en GitHub<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
               </div>
             </div>
         </div>
@@ -54,15 +54,15 @@ function Seccion3 () {
             <div className="links-proyectos link-web">
               <p className="link-proyecto2">Ir a la página web</p>
               <div className="contenedor-links">
-                <a href="https://mundial2022-gjc.herokuapp.com/" target="blank7" className="link-proyecto link-doble">(backend)<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
-                <a href="https://mundial2022-gjc.netlify.app/" target="blank7" className="link-proyecto link-doble">(frontend)<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
+                <a href="https://mundial2022-gjc.herokuapp.com/" target="blank9" className="link-proyecto link-doble">(backend)<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
+                <a href="https://mundial2022-gjc.netlify.app/" target="blank10" className="link-proyecto link-doble">(frontend)<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
               </div>
             </div>
             <div className="links-proyectos links-proyectos3">
               <p className="link-proyecto2">Ver código en GitHub</p>
               <div className="contenedor-links">
-                <a href="https://github.com/GonzaloCepeda9/gjc-mundial2022-backend" target="blank" className="link-proyecto link-doble">(backend)<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
-                <a href="https://github.com/GonzaloCepeda9/gjc-mundial2022-frontend" target="blank" className="link-proyecto link-doble">(frontend)<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
+                <a href="https://github.com/GonzaloCepeda9/gjc-mundial2022-backend" target="blank11" className="link-proyecto link-doble">(backend)<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
+                <a href="https://github.com/GonzaloCepeda9/gjc-mundial2022-frontend" target="blank12" className="link-proyecto link-doble">(frontend)<FontAwesomeIcon className="icon-link" icon={faExternalLinkAlt} /></a>
               </div>
             </div>
           </div>
